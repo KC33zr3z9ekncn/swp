8e9e689a8bb5096a3c99c500269863f6b03a9877
KC33zr3z9ekncn <106238184+KC33zr3z9ekncn@users.noreply.github.com>
2023-06-05 17:45:58 +0200

    Update views.py

--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 from flask_login import login_required
 from flask_login import current_user
 from flask_user import roles_required
-from app.models import Date, Attendance
+from app.models import Date, Attendance, Module
 from app.forms import DateForm
 
 # define the blueprint
@@ -18,14 +18,22 @@ bp_user = Blueprint('user-view', __name__, template_folder='pages', static_folde
 @roles_required('Benutzer')
 def home():
     attendances = Attendance.objects(user=current_user)
-    return render_template('user_page.html', kurse=attendances, current_user=current_user, info=session)
+    available_courses = Date.objects.all()
+    for course in available_courses:
+        course.participants = Attendance.objects(class_ref=course).count()
+    return render_template('user_page.html', kurse=attendances, available_courses=available_courses, current_user=current_user, info=session)
 
 @bp_user.route('/print', methods=['POST'])
 def handle_print():
     flash('Ihre Datei wird gedruckt.', 'success')
     return redirect(url_for('user-view.home'))
 
-@bp_user.route('/print', methods=['POST'])
+@bp_user.route('/done', methods=['POST'])
 def handle_done():
     flash('Sie nehmen am Kurs teil.', 'success')
+    return redirect(url_for('user-view.home'))
+
+@bp_user.route('/close', methods=['POST'])
+def handle_close():
+    flash('Sie haben den Kurs verlassen.', 'success')
     return redirect(url_for('user-view.home'))
